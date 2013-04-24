5908ab1ba71e1345371a4fe4dd677ef8733473e8
bali.raghav@gmail.com <bali.raghav@gmail.com@dd60caeb-59f9-1eab-ea52-cc873bf5816e>
2013-04-24 09:27:59 +0000

    Multilevel Menu Implemented

--- trunk/eBayPrototype/src/iiitb/ebay/service/MenuService.java
+++ trunk/eBayPrototype/src/iiitb/ebay/service/MenuService.java
@@ -20,23 +20,30 @@ public class MenuService {
 	public static ArrayList<Menu> fetchMenu() {
 		Connection connectionCategories;
 		Connection connectionSubCategories;
+		Connection connectionSubSubCategories;
 		ResultSet resultSetCategories;
 		ResultSet resultSetSubCategories;
+		ResultSet resultSetSubSubCategories;
 		ArrayList<Menu> menuList = new ArrayList<Menu>();
 		Menu menuItem;
 		CategoryHierarchy prodhDetailsCategories;
 		CategoryHierarchy prodhDetailsSubCategories;
+		CategoryHierarchy prodhDetailsSubSubCategories;
+		CategoryHierarchy prodhDetailsSubSubSubCategories;
 		Map<String, String> subCategoryList;
-		
-		
+		Map<String, String> subSubCategoryList;
+
+		/* More nesting */
+		Menu subMenuItem;
+		Map<String, Menu> childCategory;
+
 		try {
 			connectionCategories = DB.getConnection();
 			connectionSubCategories = DB.getConnection();
+			connectionSubSubCategories = DB.getConnection();
 
 			resultSetCategories = DB.readFromDB(fetchCategories,
 					connectionCategories);
-			
-			//callSuhas();
 
 			/* iterate all the Categories */
 			while (resultSetCategories.next()) {
@@ -57,12 +64,13 @@ public class MenuService {
 				/* Populate the menuItem */
 				menuItem.setBaseCategory(prodhDetailsCategories.getName());
 				menuItem.setPhKey(prodhDetailsCategories.getPhKey());
-				
+				menuItem.setHasChild(prodhDetailsCategories.getHasChild());
+
 				/* Fetch SubCategories only if they exist */
 				if (prodhDetailsCategories.getHasChild().equalsIgnoreCase("Y")) {
 
 					subCategoryList = new HashMap<String, String>();
-
+					childCategory = new HashMap<String, Menu>();
 					resultSetSubCategories = DB.readFromDB(fetchSubCategories
 							+ "'" + prodhDetailsCategories.getPhKey() + "'",
 							connectionSubCategories);
@@ -70,26 +78,81 @@ public class MenuService {
 
 						/* Fetch Data */
 						prodhDetailsSubCategories = new CategoryHierarchy();
-						prodhDetailsSubCategories.setPhKey(resultSetSubCategories
-								.getString("phKey"));
-						//System.out.println("phKey = "+prodhDetailsSubCategories.getPhKey());
-						prodhDetailsSubCategories.setName(resultSetSubCategories
-								.getString("name"));
-						//System.out.println("Name = "+prodhDetailsSubCategories.getName());
+						prodhDetailsSubCategories
+								.setPhKey(resultSetSubCategories
+										.getString("phKey"));
+						prodhDetailsSubCategories
+								.setName(resultSetSubCategories
+										.getString("name"));
 						prodhDetailsSubCategories
 								.setHasChild(resultSetSubCategories
 										.getString("hasChild"));
-						prodhDetailsSubCategories.setParent(resultSetSubCategories
-								.getString("parent"));
+						prodhDetailsSubCategories
+								.setParent(resultSetSubCategories
+										.getString("parent"));
 
 						/* Add to list */
+
 						subCategoryList.put(
 								prodhDetailsSubCategories.getPhKey(),
 								prodhDetailsSubCategories.getName());
+
+						if (prodhDetailsSubCategories.getHasChild()
+								.equalsIgnoreCase("Y")) {
+
+							/* Another level of nesting */
+							subMenuItem = new Menu();
+							subMenuItem
+									.setBaseCategory(prodhDetailsSubCategories
+											.getName());
+							subMenuItem.setPhKey(prodhDetailsSubCategories
+									.getPhKey());
+							subMenuItem.setHasChild(prodhDetailsSubCategories
+									.getHasChild());
+
+							subSubCategoryList = new HashMap<String, String>();
+							resultSetSubSubCategories = DB.readFromDB(
+									fetchSubCategories
+											+ "'"
+											+ prodhDetailsSubCategories
+													.getPhKey() + "'",
+									connectionSubSubCategories);
+
+							while (resultSetSubSubCategories.next()) {
+
+								/* Fetch Data */
+								prodhDetailsSubSubSubCategories = new CategoryHierarchy();
+								prodhDetailsSubSubSubCategories
+										.setPhKey(resultSetSubSubCategories
+												.getString("phKey"));
+								prodhDetailsSubSubSubCategories
+										.setName(resultSetSubSubCategories
+												.getString("name"));
+								prodhDetailsSubSubSubCategories
+										.setHasChild(resultSetSubSubCategories
+												.getString("hasChild"));
+								prodhDetailsSubSubSubCategories
+										.setParent(resultSetSubSubCategories
+												.getString("parent"));
+
+								/* Add to list */
+								subSubCategoryList.put(
+										prodhDetailsSubSubSubCategories
+												.getPhKey(),
+										prodhDetailsSubSubSubCategories
+												.getName());
+							}
+							subMenuItem.setSubcategory(subSubCategoryList);
+							childCategory.put(
+									prodhDetailsSubCategories.getPhKey(),
+									subMenuItem);
+						}
+
 					}
 
 					/* Set the categorylist for the Main Product Category */
 					menuItem.setSubcategory(subCategoryList);
+					menuItem.setChildCategory(childCategory);
 				} else {
 					menuItem.setSubcategory(null);
 				}
@@ -99,8 +162,10 @@ public class MenuService {
 			}
 
 			/* Close the connections */
+			connectionSubSubCategories.close();
 			connectionSubCategories.close();
 			connectionCategories.close();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -109,8 +174,6 @@ public class MenuService {
 		return menuList;
 	}
 
-	
-	
 	/* fetch the subcategory for the input Category key */
 	public static Map<String, String> fetchSubCategories(String phKey) {
 
@@ -121,17 +184,17 @@ public class MenuService {
 
 		try {
 			connectionSubCategories = DB.getConnection();
-			
+
 			/* fetch the resultSet */
 			resultSetSubCategories = DB.readFromDB(fetchSubCategories + "'"
 					+ phKey + "'", connectionSubCategories);
-			
+
 			/* Iterate overall the items and populate the list */
 			while (resultSetSubCategories.next()) {
 				subCategoryList.put(resultSetSubCategories.getString("phKey"),
 						resultSetSubCategories.getString("name"));
 			}
-			
+
 			connectionSubCategories.close();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -140,11 +203,147 @@ public class MenuService {
 
 		return subCategoryList;
 	}
-/*	
-	public static void callSuhas(){
-		InformSellerService inf =new InformSellerService();
-		 inf.getOutOfStockProduct(1);
-		//System.out.println("value="+lst.get(0));
+
+	/*
+	 * public static void callSuhas(){ InformSellerService inf =new
+	 * InformSellerService(); inf.getOutOfStockProduct(1);
+	 * //System.out.println("value="+lst.get(0)); }
+	 */
+
+
+	/* Fetch the entire menu from the hierarchy */
+	public static ArrayList<Menu> fetchMultiLevelMenu() {
+		Connection connectionCategories;
+
+		ResultSet resultSetCategories;
+
+		ArrayList<Menu> menuList = new ArrayList<Menu>();
+		CategoryHierarchy prodhDetailsCategories;
+		Menu menuItem;
+
+		try {
+			connectionCategories = DB.getConnection();
+
+			resultSetCategories = DB.readFromDB(fetchCategories,
+					connectionCategories);
+
+			/* iterate all the Categories */
+			while (resultSetCategories.next()) {
+
+				menuItem = new Menu();
+
+				/* Fetch Data For Top level Category */
+				prodhDetailsCategories = new CategoryHierarchy();
+				prodhDetailsCategories.setPhKey(resultSetCategories
+						.getString("phKey"));
+				prodhDetailsCategories.setName(resultSetCategories
+						.getString("name"));
+				prodhDetailsCategories.setHasChild(resultSetCategories
+						.getString("hasChild"));
+				prodhDetailsCategories.setParent(resultSetCategories
+						.getString("parent"));
+
+				/* Populate the menuItem */
+				menuItem.setBaseCategory(prodhDetailsCategories.getName());
+				menuItem.setPhKey(prodhDetailsCategories.getPhKey());
+				menuItem.setHasChild(prodhDetailsCategories.getHasChild());
+
+				/* Fetch SubCategories only if they exist */
+				if (prodhDetailsCategories.getHasChild().equalsIgnoreCase("Y")) {
+					/* Fetch SubCategory Details */
+					fetchsubMenu(prodhDetailsCategories.getPhKey(), menuItem);
+
+					/* Iterate all subcategories */
+					for (Map.Entry<String, Menu> level2 : menuItem
+							.getChildCategory().entrySet()) {
+						Menu menuObjLevel2 = level2.getValue();
+						/* Check if SubCategory has further level */
+						if (menuObjLevel2.getHasChild().equalsIgnoreCase("Y")) {
+							fetchsubMenu(menuObjLevel2.getPhKey(),
+									menuObjLevel2);
+							/* Iterate Level3 */
+							for (Map.Entry<String, Menu> level3 : menuObjLevel2
+									.getChildCategory().entrySet()) {
+								Menu menuObjLevel3 = level3.getValue();
+								/* Chekc if Level 3 has data */
+								if (menuObjLevel3.getHasChild()
+										.equalsIgnoreCase("Y")) {
+									fetchsubMenu(menuObjLevel3.getPhKey(),
+											menuObjLevel3);
+								}
+
+							}
+
+						}
+					}
+
+				} else {
+					menuItem.setSubcategory(null);
+				}
+
+				/* Add menuItem to the menuList */
+				menuList.add(menuItem);
+			}
+
+			/* Close the connections */
+			connectionCategories.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return menuList;
 	}
-*/
+	
+	
+	public static void fetchsubMenu(String phKey, Menu menuItem) {
+
+		CategoryHierarchy prodhDetailsSubCategories;
+		Map<String, Menu> childCatMenuList = new HashMap<String, Menu>();
+		Map<String, String> childCatStringList = new HashMap<String, String>();
+
+		try {
+			Connection connectionSubCategories = DB.getConnection();
+			ResultSet resultSetSubCategories = DB.readFromDB(fetchSubCategories
+					+ "'" + phKey + "'", connectionSubCategories);
+
+			while (resultSetSubCategories.next()) {
+
+				Menu subMenuItem = new Menu();
+
+				/* Fetch Data */
+				prodhDetailsSubCategories = new CategoryHierarchy();
+				prodhDetailsSubCategories.setPhKey(resultSetSubCategories
+						.getString("phKey"));
+				prodhDetailsSubCategories.setName(resultSetSubCategories
+						.getString("name"));
+				prodhDetailsSubCategories.setHasChild(resultSetSubCategories
+						.getString("hasChild"));
+				prodhDetailsSubCategories.setParent(resultSetSubCategories
+						.getString("parent"));
+
+				subMenuItem
+						.setBaseCategory(prodhDetailsSubCategories.getName());
+				subMenuItem.setPhKey(prodhDetailsSubCategories.getPhKey());
+				subMenuItem
+						.setHasChild(prodhDetailsSubCategories.getHasChild());
+
+
+				/* Add to list */
+				childCatMenuList.put(prodhDetailsSubCategories.getPhKey(),
+						subMenuItem);
+				childCatStringList.put(prodhDetailsSubCategories.getPhKey(),
+						prodhDetailsSubCategories.getName());
+
+			}
+			menuItem.setSubcategory(childCatStringList);
+			menuItem.setChildCategory(childCatMenuList);
+			connectionSubCategories.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
